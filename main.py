0d48b158d1efcaa94431d2f1cf729b6f6e1350c1
marc <marcsagabcn@gmail.com>
2023-08-08 23:10:30 +0200

    split send email function to divide responsabilities

--- main.py
+++ main.py
@@ -1,35 +1,69 @@
-import yagmail  
-import requests
+"""Script that sends an email with a cat gif and a quote of the day"""
 import os
+import yagmail
+import requests
 
 
-FILE_NAME = 'cat.gif'
+CAT_GIF_FILE = 'cat.gif'
 QUOTE_API_URL = 'https://api.api-ninjas.com/v1/quotes?category=leadership'
 CAT_API_URL = 'https://cataas.com/cat/gif'
 
-contents = ['<h3>Buenísimos días celi</h3>']
 
-def sendEmail(sender_email, sender_password, receiver_email, quote_api_key):
-    
-    r = requests.get(CAT_API_URL)
-    if r.ok:
-        with open (FILE_NAME, 'wb') as outfile:
-            outfile.write(r.content)
-            contents.append(FILE_NAME)
+def get_cat_gif():
+    """Retrieve a cat gif and save it to a local file."""
+    cat_response = requests.get(CAT_API_URL, timeout=10)
+    cat_response.raise_for_status()
 
-    response = requests.get(QUOTE_API_URL, headers={'X-Api-Key': quote_api_key})
-    if response.status_code == requests.codes.ok:
-        quoteInfo = response.json()[0]
-        contents.append("<br/><h4>Quote of the day is:</h4>")
-        contents.append('<p><i>"{}"</i> <span>- {}</span></p>'.format(quoteInfo['quote'], quoteInfo['author']))
-    else:
-        print(r)
+    with open(CAT_GIF_FILE, 'wb') as outfile:
+        outfile.write(cat_response.content)
+
+
+def get_quote_of_the_day(quote_api_key):
+    """Retrieve the quote of the day."""
+    headers = {'X-Api-Key': quote_api_key}
+    quote_response = requests.get(QUOTE_API_URL, headers=headers, timeout=10)
+    quote_response.raise_for_status()
+
+    quote_info = quote_response.json()[0]
+    return quote_info['quote'], quote_info['author']
+
+
+def send_email(sender_email, sender_password, receiver_email, subject, author, quote):
+    """Send an email with the cat gif and the quote of the day."""
+
+    contents = [
+        f'<h3>{subject}</h3>',
+        "<br/><h4>Quote of the day is:</h4>",
+        f'<p><i>"{quote}"</i> <span>- {author}</span></p>'
+    ]
 
     yag = yagmail.SMTP(sender_email, sender_password)
-    yag.send(to = receiver_email, subject="Urgent email, read as soon as possible", contents=contents)
+
+    yag.send(
+        to=receiver_email,
+        subject=subject,
+        contents=contents,
+    )
 
 
 def main():
-    sendEmail(os.environ.get("SENDER_EMAIL"), os.environ.get("SENDER_PASSWORD"), os.environ.get("RECEIVER_EMAIL"), os.environ.get("QUOTE_API_KEY"))
+    """Main function"""
+    sender_email = os.environ.get("SENDER_EMAIL")
+    sender_password = os.environ.get("SENDER_PASSWORD")
+    receiver_email = os.environ.get("RECEIVER_EMAIL")
+    quote_api_key = os.environ.get("QUOTE_API_KEY")
+    email_subject = os.environ.get("EMAIL_SUBJECT")
+
+    try:
+        quote, author = get_quote_of_the_day(quote_api_key)
+        send_email(sender_email=sender_email, sender_password=sender_password,
+                   receiver_email=receiver_email, author=author, quote=quote, subject=email_subject)
+    except requests.RequestException as error:
+        print(f"Error occurred: {error}")
+    finally:
+        # Clean up the downloaded gif
+        if os.path.exists(CAT_GIF_FILE):
+            os.remove(CAT_GIF_FILE)
+
 
 main()
